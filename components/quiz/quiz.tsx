acd590a18c97daaaba930cf6f0bf6dd05176af84
ruslanelishaev <ruslan@byondxr.com>
2023-05-21 21:08:06 +0300

    added ynthax highlighter

--- components/quiz/quiz.tsx
+++ components/quiz/quiz.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { useQuiz } from '@/hooks/use-quiz'
 import { SingleQuiz } from '@/interfaces/quiz-interface'
-import { Box, Heading, Button } from '@chakra-ui/react'
+import { Box, Heading, Button, Code, VStack } from '@chakra-ui/react'
+import { Highlight, themes } from 'prism-react-renderer'
 
 interface QuizProps {
 	quizQuestions: SingleQuiz[]
 	title: string
 }
+
 export const Quiz = ({ title, quizQuestions }: QuizProps) => {
 	const {
 		currentQuestion,
@@ -28,12 +30,34 @@ export const Quiz = ({ title, quizQuestions }: QuizProps) => {
 		resetQuiz,
 	})
 	return (
-		<Box px="1rem">
+		<Box px="2rem">
 			<Heading as="h2" fontSize="1.4rem" textAlign="center" my="2rem">
 				{title} Quiz{' '}
 			</Heading>
 
-			<Box>{currentQuestion.question}</Box>
+			<Box mb="2rem">{currentQuestion.question}</Box>
+
+			{currentQuestion.codeBlock && (
+				<Box mb="2rem">
+					<Highlight
+						theme={themes.nightOwl}
+						code={currentQuestion.codeBlock}
+						language={currentQuestion.language.toLowerCase()}
+					>
+						{({ className, style, tokens, getLineProps, getTokenProps }) => (
+							<Box as="pre" style={style} p="2rem" borderRadius="7px">
+								{tokens.map((line, i) => (
+									<div key={i} {...getLineProps({ line })}>
+										{line.map((token, key) => (
+											<span key={key} {...getTokenProps({ token })} />
+										))}
+									</div>
+								))}
+							</Box>
+						)}
+					</Highlight>
+				</Box>
+			)}
 
 			<Button
 				onClick={() => handleAnswer(currentQuestion.options[0])}
